fec7ebdfa15f364ed6e0c3ecbcbab80d6fb266f7
eeede-q <2540825819@qq.com>
2026-07-03 13:34:53 +0800

    sc

--- app.py
+++ app.py
@@ -338,5 +338,4 @@ if __name__ == '__main__':
     print("=" * 50)
     print("  访问地址：http://127.0.0.1:5000")
     print("=" * 50)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=8080)
